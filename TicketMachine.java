e8c5c3b30e3777edf099a17b28fa36a929acdac2
Kevin <kevin.felix.04@gmsil.com>
2015-10-23 12:25:04 +0200

    Añadido metodo que imprime: Please, insert the correct amount of money

--- TicketMachine.java
+++ TicketMachine.java
@@ -84,5 +84,9 @@ public class TicketMachine
      price = newPrice; 
        
     }
-
+    public void prompt()
+    {
+     System.out.println("Please, insert the correct amount of money");
+     
+    }
 }
